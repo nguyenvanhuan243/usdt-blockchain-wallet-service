47ae6180b1d97f135f7d08d4391244521c40b3c2
Huan Nguyen <nguyenvanhuan243@gmail.com>
2024-09-15 11:49:33 +0700

    update morilis

--- service/moralis.js
+++ service/moralis.js
@@ -6,32 +6,32 @@ const ABI = require("./abi.json");
 require("dotenv").config();
 
 const options = {
-  chains: [EvmChain.ETHEREUM],
-  description: "USDT Transfers 100k",
-  tag: "usdcTransfers100k",
-  includeContractLogs: true,
-  abi: ABI,
-  topic0: ["Transfer(address,address,uint256)"],
-  webhookUrl: "https://22be-2001-2003-f58b-b400-f167-f427-d7a8-f84e.ngrok.io/webhook",
-  advancedOptions: [
-    {
-        topic0: "Transfer(address,address,uint256)",
-        filter: {
-            gt : ["value", "100000"+"".padEnd(6,"0")]
+    chains: [EvmChain.ETHEREUM],
+    description: "USDT Transfers 100k",
+    tag: "usdcTransfers100k",
+    includeContractLogs: true,
+    abi: ABI,
+    topic0: ["Transfer(address,address,uint256)"],
+    webhookUrl: "https://22be-2001-2003-f58b-b400-f167-f427-d7a8-f84e.ngrok.io/webhook",
+    advancedOptions: [
+        {
+            topic0: "Transfer(address,address,uint256)",
+            filter: {
+                gt: ["value", "100000" + "".padEnd(6, "0")]
+            }
         }
-    }
-]
+    ]
 
 };
 
 Moralis.start({
-  apiKey: process.env.MORALIS_KEY ,
+    apiKey: process.env.MORALIS_KEY,
 }).then(async () => {
-  const stream = await Moralis.Streams.add(options);
-  const { id } = stream.toJSON();
+    const stream = await Moralis.Streams.add(options);
+    const { id } = stream.toJSON();
 
-  await Moralis.Streams.addAddress({
-      id: id,
-      address: ["0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"]
-  })
+    await Moralis.Streams.addAddress({
+        id: id,
+        address: ["0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"]
+    })
 });
